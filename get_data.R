57e40915b6702de47d8b4b501ca2c255f57139f8
opencedar <ahasselwander@gmail.com>
2020-03-21 14:05:34 -0400

    fixed bug putting together ETS

--- get_data.R
+++ get_data.R
@@ -6,23 +6,23 @@ require(tidyverse)
 
 #------------------------------------------------------------------------------------------------
 
-deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
 deaths_thin <- deaths %>% gather("date", "deaths", 5:ncol(deaths))
 deaths_thin$date <- substring(deaths_thin$date, 2)
 deaths_thin$date <- lubridate::mdy(deaths_thin$date)
 
 confirmed <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-confirmed_thin <- deaths %>% gather("date", "confirmed", 5:ncol(confirmed))
+confirmed_thin <- confirmed %>% gather("date", "confirmed", 5:ncol(confirmed))
 confirmed_thin$date <- substring(confirmed_thin$date, 2)
 confirmed_thin$date <- lubridate::mdy(confirmed_thin$date)
 
 recovered <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
-recovered_thin <- deaths %>% gather("date", "recovered", 5:ncol(recovered))
+recovered_thin <- recovered %>% gather("date", "recovered", 5:ncol(recovered))
 recovered_thin$date <- substring(recovered_thin$date, 2)
 recovered_thin$date <- lubridate::mdy(recovered_thin$date)
 
-covid_ets <- merge(deaths_thin, confirmed_thin, on= colnames(confirmed)[1:5])
-covid_ets <- merge(covid, recovered_thin, on = colnamse(covid)[1:5])
+covid_ets <- merge(deaths_thin, confirmed_thin, on= colnames(confirmed_thin)[1:5])
+covid_ets <- merge(covid_ets, recovered_thin, on = colnames(covid)[1:5])
 
 write.csv(covid_ets, "covid_ets.csv")
 
---
+++ visualizations.R
@@ -0,0 +1,8 @@
+require(tidyverse)
+
+covid_graph <- covid_ets %>% gather(metric, cases, recovered:confirmed)
+covid_graph_region <- covid_graph %>% filter(Country.Region == "US", Province.State == "Maryland")
+
+ggplot(covid_graph_region, aes(x=date, y=cases)) +
+  geom_line(aes(color = metric), size =1) +
+  theme_minimal()
